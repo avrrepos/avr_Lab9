d7ca5fb193fae7a403550b4477381edd688a47f0
Y-Berhie <Berhie.Yepeth@gmail.com>
2024-07-25 13:11:44 -0400

    Implement Decode Function

--- lab9.py
+++ lab9.py
@@ -6,3 +6,15 @@ def encode(password):
         newstr += str(int(c) + 3)
     return newstr
 
+
+def decode(password):
+    int_password = []
+    for num in password:
+        if int(num) > 2:
+            int_password.append(int(num) - 3)
+        elif int(num) <= 2:
+            int_password.append((int(num) + 10) - 3)
+    decoded_password = ""
+    for i in int_password:
+        decoded_password += str(i)
+    return decoded_password
